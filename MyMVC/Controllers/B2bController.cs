5ccc2900a95625cd5354929a992d5b209900410c
dimitris <iracleous@gmail.com>
2021-04-01 10:28:08 +0300

    Add project files.

---
+++ MyMVC/Controllers/B2bController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMVC.Controllers
+{
+    public class B2bController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Customer()
+        {
+            return View();
+        }
+    }
+}
